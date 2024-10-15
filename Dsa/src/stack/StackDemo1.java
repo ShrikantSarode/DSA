2a704481ce111720e6783dd27d746756293e6a0c
Shrikant <prathamesh.pys2002@gmail.com>
2024-10-15 16:02:40 +0530

    link

--- Dsa/src/stack/StackDemo1.java
+++ Dsa/src/stack/StackDemo1.java
@@ -6,7 +6,7 @@ public class StackDemo1 {
 
 	private int stack[], MaxSize, tos;
 
-	Scanner sc = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);gg
 
 	public void create_Stack(int size) {
 
